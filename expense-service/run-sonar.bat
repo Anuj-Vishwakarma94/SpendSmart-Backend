689173f7797f274c66167ef186b2e354e4912493
Anuj940 <anujvishwakarma9827@gmail.com>
2026-05-18 00:15:36 +0530

    [Anuj] : added expense-service files

--- expense-service/run-sonar.bat
+++ expense-service/run-sonar.bat
@@ -1,19 +1,17 @@
 @echo off
 echo ============================================
 echo  Running Tests + SonarQube Analysis
-echo  Service: expense-service
 echo ============================================
-
 set JAVA_HOME=C:\Program Files\Java\jdk-21.0.10
 set PATH=%JAVA_HOME%\bin;%PATH%
 
-"C:\Program Files\apache-maven-3.9.14-bin\apache-maven-3.9.14\bin\mvn.cmd" clean verify sonar:sonar ^
+call "C:\Program Files\apache-maven-3.9.14-bin\apache-maven-3.9.14\bin\mvn.cmd" clean verify sonar:sonar ^
   -Dsonar.projectKey=expense-service ^
   -Dsonar.projectName="expense-service" ^
   -Dsonar.host.url=http://localhost:9000 ^
   -Dsonar.token=sqp_b15752fe0d8a20bb7998e9bcce124a6c65678ccf
 
-if %errorlevel% equ 0 (
+if %ERRORLEVEL% equ 0 (
     echo.
     echo ============================================
     echo  SUCCESS: Analysis pushed to SonarQube!
